324caf5ec00614b7033407cbc7a56d0d552aa883
Riikka Korolainen <riikka.korolainen@gmail.com>
2024-11-21 14:20:15 +0200

    Experiment notes & run result db

--- code/run_tracking_server.sh
+++ code/run_tracking_server.sh
@@ -1,4 +1,4 @@
 mlflow server \
-  --backend-store-uri sqlite:///mlflow.db \
+  --backend-store-uri sqlite:///final_experiments.db \
   --default-artifact-root ./mlartifacts \
   --host 0.0.0.0 --port 8080
